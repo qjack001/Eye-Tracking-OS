d85217ede5b481121b37ae33894e138990f3ca19
Jack Guinane <jackguinane@gmail.com>
2019-03-11 21:34:37 -0400

    add function to open movies

--- script.js
+++ script.js
@@ -65,13 +65,17 @@ function decreaseBrightness()
 function playPause()
 {
     var vid = document.getElementById("video");
+    var icon = document.getElementById("play-icon");
     if((vid.currentTime < 0.1) || vid.paused || vid.ended || (vid.readyState < 3))
     {
         vid.play();
+        icon.className = "";
+        
     }
     else
     {
         vid.pause();
+        icon.className = "show";
     }
 }
 
@@ -86,3 +90,28 @@ function skipBack()
     var vid = document.getElementById("video");
     vid.currentTime -= 7;
 } 
+
+function playMovie(url, film)
+{
+    //url = "/Eye-Tracking-OS" + url; //for testing on github
+    var client = new XMLHttpRequest();
+    client.open('GET', url);
+    
+    document.getElementById(film).style = "top: -50px; left: -50px; overflow: visible; transition: all 1s ease; z-index: 2000;";
+    document.getElementById(film + "-img").style = "filter: brightness(0%); width: 100vw; height: 100vh; top: 0; left: 0; transition: width 1s ease, height 0.5s ease";
+    
+    client.onload = function() 
+    {
+        var page = client.responseText;
+        var content = page.substring(page.indexOf('<div id="content">') + 18, page.indexOf('</div> <!-- end of content -->'));
+        var title = page.substring(page.indexOf('<title>') + 7, page.indexOf('</title>'));
+        
+        setTimeout(function () 
+        {
+            document.getElementById("content").innerHTML = content;
+            window.history.pushState({}, title, url);
+            document.title = title;
+        }, 1000);
+    }
+    client.send();
+}
